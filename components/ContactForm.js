64b03be36a0bf45339c9b0e7bad351645801f390
FrranzMaurer <38557647+FrranzMaurer@users.noreply.github.com>
2018-09-09 16:39:34 +0200

    Add files via upload

---
+++ components/ContactForm.js
@@ -0,0 +1,27 @@
+var ContactForm = React.createClass({
+  propTypes: {
+    contact: React.PropTypes.object.isRequired
+  },
+
+  render: function() {
+    return (
+      <form className={'contactForm'}>
+        <input
+          type={'text'}
+          placeholder={'Imię'}
+          value={this.props.contact.firstName} />
+        <input
+          type={'text'}
+          placeholder={'Nazwisko'}
+          value={this.props.contact.lastName} />
+        <input
+          type={'email'}
+          placeholder={'Email'}
+          value={this.props.contact.email} />
+        </form>
+        <button 
+          type={'submit'}
+          className={'btn btn-primary'}> Dodaj kontakt </button>
+      )
+  },
+})
